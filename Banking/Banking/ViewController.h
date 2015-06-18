ccc8856a1d9bd0ad2537f636a87a37b2ac9ba1d4
Giordano Scalzo <giordano.scalzo@gmail.com>
2015-06-18 10:37:23 +0100

    Added Gitignore

---
+++ Banking/Banking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Banking
+//
+//  Created by Giordano Scalzo on 18/06/2015.
+//  Copyright (c) 2015 CrowdMix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Banking/Pods/Target Support Files/Pods-BankingTests-OCMock/Pods-BankingTests-OCMock-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BankingTests_OCMock : NSObject
+@end
+@implementation PodsDummy_Pods_BankingTests_OCMock
+@end
